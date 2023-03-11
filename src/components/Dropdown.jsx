09dff19e8aad0d05f0e5b8c603c9bd9831899da2
saerom <sagesaeromkim@gmail.com>
2023-03-11 13:55:35 +0900

    Refactor: 코드 수정 삭제

--- src/components/Dropdown.jsx
+++ src/components/Dropdown.jsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { useAuthContext } from '../context/AuthContext';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Dropdown() {
-    const { user, login, logout } = useAuthContext();
-    return (
-        <div className='flex flex-col justify-center items-center my-5 p-2'>
-            <Link className='mb-2' to='/products'>NEW</Link>
-            <Link to='/products'>ALL</Link>
-            <Link to='/products'>Outer</Link>
-            <Link to='/products'>Top</Link>
-            <Link to='/products'>Bottom</Link>
-            <Link to='/products'>Acc</Link>
-        </div>
-    );
+  return (
+    <div className="flex flex-col justify-center items-center my-5 p-2">
+      <Link className="mb-2" to="/products">
+        NEW
+      </Link>
+      <Link to="/products">ALL</Link>
+      <Link to="/products">Outer</Link>
+      <Link to="/products">Top</Link>
+      <Link to="/products">Bottom</Link>
+      <Link to="/products">Acc</Link>
+    </div>
+  );
 }
-
